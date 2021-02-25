1706a0cb127f6b1f576a9c58345ec9fc6929ee5d
Mustafa Gezen <mustafa@rockylinux.org>
2021-02-25 02:24:25 +0100

    add imports route to router

--- distrobuild/routes/__init__.py
+++ distrobuild/routes/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from distrobuild.routes import builds, packages, bootstrap
+from distrobuild.routes import builds, imports, packages, bootstrap
 
 _base_router = APIRouter(prefix="/api")
 
@@ -9,5 +9,6 @@ def register_routes(app):
     _base_router.include_router(packages.router)
     _base_router.include_router(bootstrap.router)
     _base_router.include_router(builds.router)
+    _base_router.include_router(imports.router)
 
     app.include_router(_base_router)
