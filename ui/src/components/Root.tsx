4fa2a26b19c81f45114720fe1af2c63ab38a1264
Mustafa Gezen <mustafa@rockylinux.org>
2021-04-06 19:28:22 +0200

    Capitalize authors

--- ui/src/components/Root.tsx
+++ ui/src/components/Root.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Distrobuild authors
+ * Copyright (c) 2021 The Distrobuild Authors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
