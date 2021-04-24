6d5c1ec39dd7afb96a950939848a55a2ea62865f
Mustafa Gezen <mustafa@rockylinux.org>
2021-04-25 01:09:12 +0200

    Tag modular candidates only after signing

--- distrobuild_scheduler/periodic_tasks.py
+++ distrobuild_scheduler/periodic_tasks.py
@@ -82,25 +82,25 @@ async def atomic_sign_unsigned_builds(build: Build):
 
             build_rpms = koji_session.listBuildRPMs(rpm["nvr"])
             if len(build_rpms) > 0:
-                package_modules = await PackageModule.filter(
-                    module_parent_package_id=build.package.id).prefetch_related(
-                    "package").all()
-                for package_module in package_modules:
-                    package_module_package = await Package.filter(id=package_module.package.id).first()
-                    package_module_package.last_build = datetime.datetime.now()
-                    await package_module_package.save()
-
-                    if package_module.package.repo != Repo.MODULAR_CANDIDATE:
-                        continue
-                    koji_session.packageListAdd(tags.module_compose(), package_module.package.name, "distrobuild")
-                    koji_package = koji_session.getPackage(package_module.package.name)
-                    koji_builds = koji_session.listBuilds(koji_package["id"])
-                    for koji_build in koji_builds:
-                        if koji_build["source"] == mbs_build["scmurl"]:
-                            tag_if_not_tagged([], koji_build["nvr"], tags.module_compose())
-                            break
-
                 await sign_build_rpms(build_rpms)
+
+        package_modules = await PackageModule.filter(
+            module_parent_package_id=build.package.id).prefetch_related(
+            "package").all()
+        for package_module in package_modules:
+            package_module_package = await Package.filter(id=package_module.package.id).first()
+            package_module_package.last_build = datetime.datetime.now()
+            await package_module_package.save()
+
+            if package_module.package.repo != Repo.MODULAR_CANDIDATE:
+                continue
+            koji_session.packageListAdd(tags.module_compose(), package_module.package.name, "distrobuild")
+            koji_package = koji_session.getPackage(package_module.package.name)
+            koji_builds = koji_session.listBuilds(koji_package["id"])
+            for koji_build in koji_builds:
+                if koji_build["source"] == mbs_build["scmurl"]:
+                    tag_if_not_tagged([], koji_build["nvr"], tags.module_compose())
+                    break
 
         build.signed = True
         await build.save()
